a1a32e89353e21fcd433ee01919c3db1a094f43a
Nathanael Johnson <nathanealjohnson@gmail.com>
2022-10-06 06:18:20 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Project--1---Connect---4
+Connect Four
+
+A two-player connection board game, in which the players choose a color and then take turns dropping colored tokens into a seven-column, six-row vertically suspended grid. The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own tokens.The first player can always win by playing the right moves.
+
--- script.js
+++ script.js
@@ -1,36 +1,102 @@
-let allCells = document.querySelectorAll('.cell:not(.row-top)');
-let topCells = document.querySelectorAll('.cell.row-top');
-let resetButton = document.querySelector('.reset');
-let statusSpan = document.querySelector('.status');
+let board=document.querySelector(".board") 
+let player=document.querySelector(".player") 
+let playAgain=document.querySelector(".playAgain") 
+let restart=document.querySelector(".restart") 
+let box=0 
+let winningArray = [ 
+[0, 1, 2, 3], [41, 40, 39, 38],[7, 8, 9, 10], 
+[34, 33, 32, 31], [14, 15, 16, 17], [27, 26, 25, 24], 
+[21, 22, 23, 24], [20, 19, 18, 17], [28, 29, 30, 31], 
+[13, 12, 11, 10], [35, 36, 37, 38], [6, 5, 4, 3], 
+[0, 7, 14, 21], [41, 34, 27, 20], [1, 8, 15, 22], 
+[40, 33, 26, 19], [2, 9, 16, 23], [39, 32, 25, 18], 
+[3, 10, 17, 24], [38, 31, 24, 17], [4, 11, 18, 25], 
+[37, 30, 23, 16], [5, 12, 19, 26], [36, 29, 22, 15], 
+[6, 13, 20, 27], [35, 28, 21, 14], [0, 8, 16, 24], 
+[41, 33, 25, 17], [7, 15, 23, 31], [34, 26, 18, 10], 
+[14, 22, 30, 38], [27, 19, 11, 3], [35, 29, 23, 17], 
+[6, 12, 18, 24], [28, 22, 16, 10], [13, 19, 25, 31], 
+[21, 15, 9, 3], [20, 26, 32, 38], [36, 30, 24, 18], 
+[5, 11, 17, 23], [37, 31, 25, 19], [4, 10, 16, 22], 
+[2, 10, 18, 26], [39, 31, 23, 15], [1, 9, 17, 25], 
+[40, 32, 24, 16], [9, 7, 25, 33], [8, 16, 24, 32], 
+[11, 7, 23, 29], [12, 18, 24, 30], [1, 2, 3, 4], 
+[5, 4, 3, 2], [8, 9, 10, 11], [12, 11, 10, 9],
+[15, 16, 17, 18], [19, 18, 17, 16], [22, 23, 24, 25], 
+[26, 25, 24, 23], [29, 30, 31, 32], [33, 32, 31, 30], 
+[36, 37, 38, 39], [40, 39, 38, 37], [7, 14, 21, 28], 
+[8, 15, 22, 29], [9, 16, 23, 30], [10, 17, 24, 31], 
+[11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34] 
+]; 
+let currentPlayer=1 
 
-// columns
-const column0 = [allCells[35], allCells[28], allCells[21], allCells[14], allCells[7], allCells[0], topCells[0]];
-const column1 = [allCells[36], allCells[29], allCells[22], allCells[15], allCells[8], allCells[1], topCells[1]];
-const column2 = [allCells[37], allCells[30], allCells[23], allCells[16], allCells[9], allCells[2], topCells[2]];
-const column3 = [allCells[38], allCells[31], allCells[24], allCells[17], allCells[10], allCells[3], topCells[3]];
-const column4 = [allCells[39], allCells[32], allCells[25], allCells[18], allCells[11], allCells[4], topCells[4]];
-const column5 = [allCells[40], allCells[33], allCells[26], allCells[19], allCells[12], allCells[5], topCells[5]];
-const column6 = [allCells[41], allCells[34], allCells[27], allCells[20], allCells[13], allCells[6], topCells[6]];
-const columns = [column0, column1, column2, column3, column4, column5, column6];
 
 
-// rows
-const topRow = [topCells[0], topCells[1], topCells[2], topCells[3], topCells[4], topCells[5], topCells[6]];
-const row0 = [allCells[0], allCells[1], allCells[2], allCells[3], allCells[4], allCells[5], allCells[6]];
-const row1 = [allCells[7], allCells[8], allCells[9], allCells[10], allCells[11], allCells[12], allCells[13]];
-const row2 = [allCells[14], allCells[15], allCells[16], allCells[17], allCells[18], allCells[19], allCells[20]];
-const row3 = [allCells[21], allCells[22], allCells[23], allCells[24], allCells[25], allCells[26], allCells[27]];
-const row4 = [allCells[28], allCells[29], allCells[30], allCells[31], allCells[32], allCells[33], allCells[34]];
-const row5 = [allCells[35], allCells[36], allCells[37], allCells[38], allCells[39], allCells[40], allCells[41]];
-const rows = [row0, row1, row2, row3, row4, row5, topRow];
+document.addEventListener("DOMContentLoaded", loadDOM)
 
 
+function loadDOM(){ 
+    createBoard() 
+    player.innerHTML=currentPlayer 
+    let squares =document.querySelectorAll(".board div") 
+    Array.from(squares).forEach(square=>{ 
+    square.addEventListener("click",clickBox)
+    })
+    playAgain.addEventListener("click",reset) 
+    }
 
-let gameIsLive = true;
-let yellowIsNext = true;
+    function createBoard(){ 
+        for(let i=0;i<49;i++){ 
+        let div =document.createElement("div") 
+        div.setAttribute("data-id",i) 
+        div.className = "square" 
+        if (i>=42){ 
+        div.className="taken" 
+        } 
+        board.appendChild(div) 
+        } 
+        }  
 
-let getClassListArray = (cell) => {
-    const getList = cell.classlist;
-    return[...classlist];
-};
+        function clickBox(){ 
+            let squares =document.querySelectorAll(".board div") 
+            let click =parseInt(this.dataset.id) 
+            if( squares[click+7].classList.contains("taken") && !squares[click].classList.contains("taken")){ 
+            if(currentPlayer===1){ 
+            currentPlayer=2 
+            player.innerHTML=currentPlayer
+            this.className="player-one taken" 
+            checkWon() 
+            }else if(currentPlayer===2){ 
+            currentPlayer=1 
+            player.innerHTML=currentPlayer 
+            this.className="player-two taken" 
+            checkWon() 
+            } 
+            if(box===42){ 
+            setTimeout(()=>alert("boxes filled"),300)
+            setTimeout(()=>restart.style.display="flex",500) 
+            } 
+            
+            } 
+            }
 
+            function checkWon(){
+                let squares =document.querySelectorAll(".board div")
+                for (let i=0;i<winningArray.length;i++){
+                let square =winningArray[i]
+                if(square.every(q=>squares[q].classList.contains("player-one"))){
+                  setTimeout(() =>alert("Red wins "), 200)
+                  setTimeout(() =>restart.style.display="flex", 500)
+                  }else if(square.every(q=>squares[q].classList.contains("player-two"))){
+                  setTimeout(() =>alert("Yellow wins"), 200)
+                  setTimeout(() =>restart.style.display="flex", 500)
+                }
+                }
+                }
+
+
+                function reset(){
+                    board.innerHTML="" 
+                    loadDOM() 
+                    restart.style.display="none" 
+                    }
